3a7baed61a574e4aa002578da3a55e3494d5391e
hentho <henrik.thoresson@gmail.com>
2014-09-24 10:27:44 +0200

    Signed-off-by: hentho <henrik.thoresson@gmail.com>

---
+++ output.go
@@ -0,0 +1,61 @@
+
+package main
+
+import (
+  "fmt"
+)
+
+func printNode(node *DHTnode) {
+
+  fmt.Printf("node id: %s\n", node.nodeId)
+
+  /*
+  fmt.Printf("predecessor1 id: %s\n", node.predecessors[1].nodeId)
+  fmt.Printf("predecessor0 id: %s\n", node.predecessors[0].nodeId)
+  
+  fmt.Printf("node id          : %s\n", node.nodeId)
+  fmt.Printf("successor0 id    : %s\n", node.successors[0].nodeId)
+  fmt.Printf("Finger   1    key: %s\n", node.fingers[0].key)  
+  fmt.Printf("Finger   1 nodeId: %s\n", node.fingers[0].nodeId)
+  fmt.Printf("Finger   3    key: %s\n", node.fingers[2].key)
+  fmt.Printf("Finger   3 nodeId: %s\n", node.fingers[2].nodeId) 
+  
+  fmt.Printf("Finger  80 nodeId: %s\n", node.fingers[79].nodeId)
+  fmt.Printf("Finger  80    key: %s\n\n", node.fingers[79].key)
+  fmt.Printf("Finger 130 nodeId: %s\n", node.fingers[129].nodeId)
+  fmt.Printf("Finger 130    key: %s\n\n", node.fingers[129].key)
+  
+  
+  fmt.Printf("Finger 155    key: %s\n", node.fingers[154].key)
+  fmt.Printf("Finger 155 nodeId: %s\n", node.fingers[154].nodeId)
+  fmt.Printf("Finger 160    key: %s\n", node.fingers[159].key)
+  fmt.Printf("Finger 160 nodeId: %s\n", node.fingers[159].nodeId)
+  
+  fmt.Printf("nodeIp: %s\n", node.nodeIp)
+  fmt.Printf("nodePort: %s\n", node.nodePort)
+  fmt.Printf("joinViaIp: %s\n", node.joinViaIp)
+  fmt.Printf("joinViaPort: %s\n\n", node.joinViaPort)
+  fmt.Printf("successor0 id: %s\n", node.successors[0].nodeId)
+  fmt.Printf("successor1 id: %s\n", node.successors[1].nodeId)
+  fmt.Printf("successorIp: %s\n", self.successors[0].nodeIp)
+  fmt.Printf("successorPort: %s\n\n", self.successors[0].nodePort)
+  */
+
+  //fmt.Printf("\n")
+}
+
+
+func (self *DHTnode) printRing(){
+	fmt.Println("\nNodes in ring:")
+ 	start := self
+ 	  
+    printNode(self)
+
+  	self = self.successor
+  	for(self != start){
+
+      printNode(self)
+
+      self = self.successor
+  	}
+ }
---
+++ util.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+	"crypto/sha1"
+	"math/big"
+)
+
+
+func sha1hash(str string) string {
+	// calculate sha-1 hash
+	hasher := sha1.New()
+	hasher.Write([]byte(str))
+
+	return fmt.Sprintf("%x", hasher.Sum(nil))
+}
+
+
+func distance(a, b []byte, bits int) *big.Int {
+	var ring big.Int
+	ring.Exp(big.NewInt(2), big.NewInt(int64(bits)), nil)
+
+	var a_int, b_int big.Int
+	(&a_int).SetBytes(a)
+	(&b_int).SetBytes(b)
+
+	var dist big.Int
+	(&dist).Sub(&b_int, &a_int)
+
+	(&dist).Mod(&dist, &ring)
+	return &dist
+}
+
+
+func between(id1, id2, key []byte) bool {
+	// 0 if a==b, -1 if a < b, and +1 if a > b
+
+	if bytes.Compare(key, id1) == 0 { // key == id1
+		return true
+	}
+
+	if bytes.Compare(id2, id1) == 1 { // id2 > id1
+		if bytes.Compare(key, id2) == -1 && bytes.Compare(key, id1) == 1 { // key < id2 && key > id1
+			return true
+		} else {
+			return false
+		}
+	} else { // id1 > id2
+		if bytes.Compare(key, id1) == 1 || bytes.Compare(key, id2) == -1 { // key > id1 || key < id2
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+
+// (n + 2^(k-1)) mod (2^m)
+func calcFinger(n []byte, k int, m int) (string) {
+//func calcFinger(n []byte, k int, m int) (string, []byte) {  // original version has two return values, do we need those?
+
+	// convert the n to a bigint
+	nBigInt := big.Int{}
+	nBigInt.SetBytes(n)
+
+	// get the right addend, i.e. 2^(k-1)
+	two := big.NewInt(2)
+	addend := big.Int{}
+	addend.Exp(two, big.NewInt(int64(k-1)), nil)
+
+	// calculate sum
+	sum := big.Int{}
+	sum.Add(&nBigInt, &addend)
+
+	// calculate 2^m
+	ceil := big.Int{}
+	ceil.Exp(two, big.NewInt(int64(m)), nil)
+
+	// apply the mod
+	result := big.Int{}
+	result.Mod(&sum, &ceil)
+
+
+	resultBytes := result.Bytes()
+	resultHex := fmt.Sprintf("%x", resultBytes)
+
+	//return resultHex, resultBytes
+	return resultHex
+}
+
+

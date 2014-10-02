6ddd794f891402c9a476cf2a1086dcd4a58da58a
hentho <henrik.thoresson@gmail.com>
2014-10-02 14:12:13 +0200

    Signed-off-by: hentho <henrik.thoresson@gmail.com>

---
+++ standalone_node.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+/*
+/ When finished this file should be able to replace ring.go
+/ A single node is created, the node stars to lisen for connection attempts
+/ from ohter node, or makes attempt to connect other node after port for 
+/ other node is specified.
+/ nodeIP is localhost for all nodes.
+*/
+
+func main() {
+	
+	fmt.Printf("New node is starting...\n")
+
+	fmt.Printf("Port for this node: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		{break}
+	}
+	thisNodePort := scanner.Text()
+	
+	port,_ := strconv.Atoi(thisNodePort)
+	
+	thisNode := createNode(port)
+
+	//thisNode.updateAllFingerTables()
+
+	thisNode.printRing()
+	
+	// start Listener
+
+	fmt.Printf("Connect to node on port: ")
+	scanner = bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		{break}
+	}
+	remoteNodePort := scanner.Text()
+
+	fmt.Printf(remoteNodePort)
+	fmt.Printf(thisNodePort)
+
+	// sendMessage(join, localhost, remoteNodePort)
+
+
+	
+}

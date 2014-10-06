1d7a265a369213b7210b01f41988481fa6533c21
Titotix <titotix@free.fr>
2014-10-06 13:35:18 +0200

    enabled rpc for Lookup method + more

---
+++ rpc.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+type ArgLookup struct {
+	Node DHTnode
+	Key  string
+}
+
+/*
+Abstract RPC for Lookup method
+@arg : ArgLookup{nodeTarget.Successor, keyTarget}
+With nodeTarget.Successor is the node which are going to respond to this rpc
+keyTarget is the key which we are looking for
+*/
+func (self *DHTnode) callLookup(clientSocket *rpc.Client, arg *ArgLookup) *DHTnode {
+
+	var reply DHTnode
+	err := clientSocket.Call("DHTnode.Lookup", arg, &reply)
+	if err != nil {
+		log.Fatal("remote lookup error:", err)
+	}
+	fmt.Printf("reply : %s", reply.NodeId)
+	return &reply
+}
+
+//Abstract callLookup method
+// nodeTarget is the node where rpc is computed
+func (nodeTarget *DHTnode) lookup(keyTarget string) *DHTnode {
+
+	arg := ArgLookup{*nodeTarget, keyTarget}
+	clientSocket := nodeTarget.connect(arg.Node.NodeIp, arg.Node.NodePort)
+	reply := nodeTarget.callLookup(clientSocket, &arg)
+	return reply
+}
